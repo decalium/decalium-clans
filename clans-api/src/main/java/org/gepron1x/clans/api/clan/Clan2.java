91d17d64e84954554af9fa3f2c223b45fa90bde6
gepron1x <georgijpronuk@gmail.com>
2022-02-21 22:22:32 +0300

    More elegant code design start

---
+++ clans-api/src/main/java/org/gepron1x/clans/api/clan/Clan2.java
@@ -0,0 +1,15 @@
+package org.gepron1x.clans.api.clan;
+
+import org.gepron1x.clans.api.edition.ClanEdition;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public interface Clan2 {
+    int id();
+
+    @NotNull CompletableFuture<DraftClan> getDraft();
+
+    @NotNull CompletableFuture<DraftClan> edit(Consumer<ClanEdition> consumer);
+}
