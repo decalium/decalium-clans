1d7cd1e94773379652e6f76ba93dc376cdfaca8c
Георгий Пронюк <georgijpronuk@gmail.com>
2022-07-13 23:04:50 +0300

    fix reload command (i hope?)

--- clans-plugin/src/main/java/org/gepron1x/clans/plugin/chat/CarbonChatHook.java
+++ clans-plugin/src/main/java/org/gepron1x/clans/plugin/chat/CarbonChatHook.java
@@ -2,6 +2,7 @@ package org.gepron1x.clans.plugin.chat;
 
 import net.draycia.carbon.api.CarbonChat;
 import net.draycia.carbon.api.CarbonChatProvider;
+import net.draycia.carbon.api.channels.ChannelRegistry;
 import org.bukkit.Server;
 import org.gepron1x.clans.plugin.cache.ClanCacheImpl;
 import org.gepron1x.clans.plugin.config.ClansConfig;
@@ -13,7 +14,9 @@ public record CarbonChatHook(@NotNull Server server, @NotNull ClanCacheImpl cach
     public void register() {
         CarbonChat carbon = CarbonChatProvider.carbonChat();
         ClanChatChannel chatChannel = new ClanChatChannel(server, cache, messages, clansConfig);
+        ChannelRegistry registry = carbon.channelRegistry();
+        boolean exists = registry.get(chatChannel.key()) != null;
         carbon.channelRegistry().register(chatChannel.key(), chatChannel);
-        carbon.channelRegistry().registerChannelCommands(chatChannel);
+        if(!exists) carbon.channelRegistry().registerChannelCommands(chatChannel);
     }
 }
