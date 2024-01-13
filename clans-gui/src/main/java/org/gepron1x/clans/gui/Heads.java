1937381696de7ddcb50ba10a4b3ce74ac3ac16be
georgijpronuk@gmail.com <georgijpronuk@gmail.com>
2024-01-13 15:18:00 +0300

    New heads

--- clans-gui/src/main/java/org/gepron1x/clans/gui/Heads.java
+++ clans-gui/src/main/java/org/gepron1x/clans/gui/Heads.java
@@ -30,4 +30,7 @@ public final class Heads {
 	public static final PlayerProfile GREEN_CHECKMARK = fromId("a92e31ffb59c90ab08fc9dc1fe26802035a3a47c42fee63423bcdb4262ecb9b6");
 	public static final PlayerProfile DECLINE = fromId("beb588b21a6f98ad1ff4e085c552dcb050efc9cab427f46048f18fc803475f7");
 	public static final PlayerProfile GO_BACK = fromId("74133f6ac3be2e2499a784efadcfffeb9ace025c3646ada67f3414e5ef3394");
+
+	public static final PlayerProfile NEXT = fromId("f32ca66056b72863e98f7f32bd7d94c7a0d796af691c9ac3a9136331352288f9");
+	public static final PlayerProfile PREVIOUS = fromId("86971dd881dbaf4fd6bcaa93614493c612f869641ed59d1c9363a3666a5fa6");
 }
