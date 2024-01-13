d74001cf4d5dd01907bda24b65d74db55a5e2947
georgijpronuk@gmail.com <georgijpronuk@gmail.com>
2024-01-13 15:17:11 +0300

    It's a radius not a diameter i'm stupid

--- clans-plugin/src/main/java/org/gepron1x/clans/plugin/shield/region/wg/RegionCreation.java
+++ clans-plugin/src/main/java/org/gepron1x/clans/plugin/shield/region/wg/RegionCreation.java
@@ -28,12 +28,11 @@ public record RegionCreation(Configs configs, ClanRegion region) {
 	}
 
 	public static ProtectedRegion create(String name, Location location, double radius) {
-		double halfSize = radius / 2;
 		int x = location.getBlockX();
 		int y = location.getBlockY();
 		int z = location.getBlockZ();
-		BlockVector3 first = BlockVector3.at(x - halfSize, y - halfSize, z - halfSize);
-		BlockVector3 second = BlockVector3.at(x + halfSize, y + halfSize, z + halfSize);
+		BlockVector3 first = BlockVector3.at(x - radius, y - radius, z - radius);
+		BlockVector3 second = BlockVector3.at(x + radius, y + radius, z + radius);
 		return new ProtectedCuboidRegion(name, true, first, second);
 	}
 }
